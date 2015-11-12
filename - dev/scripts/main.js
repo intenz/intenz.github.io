c9515e28c338e9fe95addcdd83795a8223e372c6
Viktor <_intenz_@ukr.net>
2015-11-12 18:38:09 +0200

    loading

--- - dev/scripts/main.js
+++ - dev/scripts/main.js
@@ -320,3 +320,105 @@ $(document).ready(function() {
         console.log('yohoho');
     });
 });
+
+var myModule = (function(){
+
+//Инициализирует наш модуль
+    var init = function(){  
+        _setUpListeners();
+        //то, что должно произойти сразу
+    if ($('#fileuploadSecond').attr('disabled')) {$('#filenameSecond').css('background-color','#dcd2c7');}
+        };
+
+//Прослушивает события
+    var _setUpListeners=function(){
+        $('#fileuploadFirst').on('change', _changefileUploadFirst); //добавление файла в первый input
+        $('#fileuploadSecond').on('change', _changefileUploadSecond); //добавление файла во второй input
+        };
+
+//Изменяет файл аплоад
+  var _changefileUploadFirst = function() {
+    var input = $(this); //инпут type="file"
+        filename = input.val(); //имя загруженного элемента
+        console.log(filename);
+        filename = getNameFromPath(filename); //Передаем функции значение input
+
+        // Получаем название файла из пути
+          function getNameFromPath () {
+              return filename.replace(/\\/g, '/').replace(/.*\//, ''); //Получаем название файла из пути
+          }
+
+        $('#filenameFirst').val(filename);
+        if (filename) {$('#fileuploadSecond').removeAttr('disabled'); $('#filenameSecond').css('background-color','#f1f1f5');}//Удаляем атрибут disabled у второго инпута
+  };
+
+  var _changefileUploadSecond = function() {
+    
+    var input = $(this), //инпут type="file"
+        filename = input.val(); //имя загруженного элемента
+        filename = getNameFromPath(filename); //Передаем функции значение input
+
+        // Получаем название файла из пути
+          function getNameFromPath () {
+              return filename.replace(/\\/g, '/').replace(/.*\//, ''); //Получаем название файла из пути
+          }
+
+        $('#filenameSecond').val(filename);
+  };
+
+//Возвращаем объект(публичные методы)
+    return {
+        init:init,
+    };
+})();
+myModule.init();
+
+$(function () {
+    'use strict';
+
+    // Initialize the jQuery File Upload widget:
+    $('#fileuploadFirst').fileupload({
+
+        url: 'server/php/',
+        dataType: 'json',
+        add: function(e,data){
+            data.submit();
+
+        },
+
+        done: function(e,data){
+            /*$.each(data.result.files, function (index, file) {
+                var img = $('.img__main-uploaded'),
+                img.attr('src',file.url);
+            });*/
+            
+            var img = $('.img__main-uploaded'),
+                uploadImg=data.result.files[0];
+            img.attr('src',uploadImg.url);
+
+            //img.appendTo('.img__main-uploaded');
+
+        }
+
+    });
+
+    $('#fileuploadSecond').fileupload({
+
+        url: 'server/php/',
+        dataType: 'json',
+        add: function(e,data){
+            data.submit();
+
+        },
+
+        done: function(e,data){
+            var img = $('.img__watermark-uploaded'),
+                uploadImg=data.result.files[0];
+            //img.attr('src', data.result.files[0]['url']);
+            img.attr('src',uploadImg.url);
+
+        }
+
+    });
+
+});
